63edf173aa5a8a147705979442f081ec44420c83
PiterPetrenko <p_2_p@mail.ru>
2012-07-26 00:35:28 +0300

    Added speaking ability

---
+++ src/main/java/com/modules/social/abilities/SpeakingAbility.java
@@ -0,0 +1,9 @@
+package com.modules.social.abilities;
+
+public interface SpeakingAbility {
+	public void ask(String question);
+
+	public void say(String word);
+
+	public String answer(String quesion);
+}
